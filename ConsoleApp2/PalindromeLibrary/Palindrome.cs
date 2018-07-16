1c36404f18e33820557abf8a204233e49465af48
Joseph <josephcrusso1993@gmail.com>
2018-07-16 12:17:55 -0400

    Finished

---
+++ ConsoleApp2/PalindromeLibrary/Palindrome.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks; 
+
+
+namespace PalindromeLibrary
+{
+   
+
+    public class Palindrome
+    {
+        public  bool IsPalindrome(string word)
+        {
+            word = word.Replace(" ", "");
+            word = word.ToLower();
+            char[] arr = word.ToCharArray();
+            Array.Reverse(arr);
+            string temp = new string(arr);
+            return word.Equals(temp);
+        }
+
+    }
+}
---
+++ ConsoleApp2/UnitTestProject2/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject2")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b1b23554-b5f4-4c7e-adae-c0d0ef246f8b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
